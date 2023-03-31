e2ea222ebda91c0e66d611139cc7d469539d3306
naveenkrc2 <129486444+naveenkrc2@users.noreply.github.com>
2023-03-31 15:17:54 +0530

    Add files via upload

---
+++ naveen1.py
@@ -0,0 +1,17 @@
+import itertools
+def expenses(inp)
+dic={26:"Toi",22.5:"Hindu",34:"ET",10.5:"BM",18:"HT",};
+var=[26,22.5,34,10.5,18]
+newp=[]
+jion=list(iteratools.combination(var,2))
+for i in jion:
+if=sum(i)<=inp:
+newp.append(i)
+np[]
+for i in range(len(newp)):
+    var1=newp[i][1]
+    var2=newp[i][1]
+    np append((Dic[var1],Dic[var2]))
+    print(np)
+    inp=int(input())
+    expenses(inp)
